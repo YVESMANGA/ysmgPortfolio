8182eb23fb86c931f804b244b52480955b279ac3
Yves Manga <94999497+YVESMANGA@users.noreply.github.com>
2025-08-18 13:04:02 +0200

    Update Project.tsx

--- src/components/project/Project.tsx
+++ src/components/project/Project.tsx
@@ -10,57 +10,70 @@ const projects = [
         demoLink: 'https://ysmg-portfolio.netlify.app/',
         repoLink: 'https://github.com/YVESMANGA/ysmgPortfolio',
         image: 'SiteP.PNG',
-    }
+    },
+    {
+      title: 'Saoro Negoce',
+      description: "Site Vitrine, d'une entreprise dans le BTP", 
+      technologies: ['NextJS', 'Tailwind CSS'],
+      demoLink: 'http://saoronegoce.com/',
+      image: 'logos.JPG',
+  },
+  {
+    title: 'Modexa',
+    description: "Marketplace, permettant a d'autres boutiques, de renseigner leur produits.", 
+    technologies: ['Laravel', 'Tailwind CSS'],
+    demoLink: 'http://modexa.moda/',
+    image: 'modexa.JPG',
+  }
 ]
 
 const Project = () => {
   return (
     <div className='p-5 md:px-[15%]' id='project'>
-      <div className='mt-10'>
-        <Title title='Mes Projets'/>
+  <div className='mt-10'>
+    <Title title='Mes Projets'/>
 
-        <div>
-          {projects.map((project, index) => (
-            <div key={index} className='bg-base-300 p-5 h-fit rounded-xl shadow-lg'>
-              <img 
-                src={project.image} 
-                alt={project.title} 
-                className='w-full rounded-xl h-56 object-cover' 
-              />
+    {/* Grille responsive */}
+    <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+      {projects.map((project, index) => (
+        <div key={index} className='bg-base-300 p-5 h-fit rounded-xl shadow-lg'>
+          <img 
+            src={project.image} 
+            alt={project.title} 
+            className='w-full rounded-xl h-56 object-cover' 
+          />
 
-              <div>
-                <h1 className='my-2 font-bold'>
-                  {project.title}
-                </h1>
-                <p className='text-sm'>
-                  {project.description}
-                </p>
-              </div>
+          <div>
+            <h1 className='my-2 font-bold'>
+              {project.title}
+            </h1>
+            <p className='text-sm'>
+              {project.description}
+            </p>
+          </div>
 
-              <div className='flex flex-wrap gap-2 my-3'>
-                {project.technologies.map((tech, index) => (
-                  <span key={index} className='badge badge-accent badge-sm'>
-                    {tech}
-                  </span>
-                ))}
-              </div>
+          <div className='flex flex-wrap gap-2 my-3'>
+            {project.technologies.map((tech, index) => (
+              <span key={index} className='badge badge-accent badge-sm'>
+                {tech}
+              </span>
+            ))}
+          </div>
 
-              <div>
-                <a href={project.demoLink} className='btn btn-accent'>
-                  Demo 
-                  <Video className='w-4' />
-                </a>
+          <div>
+            <a href={project.demoLink} className='btn btn-accent'>
+              Demo 
+              <Video className='w-4' />
+            </a>
 
-                <a href={project.repoLink} className='btn btn-neutral w-1/3 ml-2'>
-                  Github 
-                  <Github className='w-4' />
-                </a>
-              </div>
-            </div>
-          ))}
+            
+          </div>
         </div>
-      </div>
+      ))}
     </div>
+  </div>
+</div>
+
   )
 }
 
